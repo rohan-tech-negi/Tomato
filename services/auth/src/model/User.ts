8f61a2a86ddad15013ca6b99e7f0369778a3149e
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-05 15:47:24 +0530

    created a model folder to make the model for the database

---
+++ services/auth/src/model/User.ts
@@ -0,0 +1,21 @@
+import mongoose , {Document, Schema} from "mongoose"
+
+export interface IUser extends Document{
+    name: string,
+    email: string,
+    password: string,
+    role: string,
+    image: string,
+}
+
+const schema: Schema<IUser> = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true , unique: true},
+    image: {type: String, required: true},
+    role: {type: String, default: null},
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model<IUser>("User", schema);
+export default User;
