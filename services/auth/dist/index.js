7f9429e9214eeab142ff679e2b5e63bd4e3068c9
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-06 18:24:20 +0530

    mongo db connected to the atlas

--- services/auth/dist/index.js
+++ services/auth/dist/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"
+import connectDB from "./config/db.js";
 
 
 dotenv.config()
@@ -9,4 +10,5 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+    connectDB();
 })
