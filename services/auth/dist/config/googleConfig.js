9a5b78ceddd88bdfd3f651d5c467f5808536f755
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-09 12:44:05 +0530

    clean commit without secrets

---
+++ services/auth/dist/config/googleConfig.js
@@ -0,0 +1,8 @@
+import { google } from 'googleapis';
+import dotenv from 'dotenv';
+dotenv.config();
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+export const oauth2client = new google.auth;
+.0;
+Auth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "authmessagees");
