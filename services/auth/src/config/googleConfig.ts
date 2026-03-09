c0a658643e9ce14897a72bf50e20a9f76be7c9bc
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-09 12:46:07 +0530

    created a googleConfig.ts to make the google cloud work with it

--- services/auth/src/config/googleConfig.ts
+++ services/auth/src/config/googleConfig.ts
@@ -1,13 +1,13 @@
-// import { google } from "googleapis";
-// import dotenv from "dotenv";
+import { google } from "googleapis";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 
-// export const oauth2Client = new google.auth.OAuth2(
-//   GOOGLE_CLIENT_ID,
-//   GOOGLE_CLIENT_SECRET,
-//   "authmessagees"
-// );
+export const oauth2Client = new google.auth.OAuth2(
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  "authmessagees"
+);
