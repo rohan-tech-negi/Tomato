c3e029812cc8f919bbf153ebe13e157c3e7d6791
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-11 02:33:15 +0530

    done with the navbar logic now working on the ui

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppData } from '../context/AppContext'
+import { useLocation, useSearchParams } from 'react-router-dom'
 
 const Navbar = () => {
-    
+    const {isAuth} = useAppData()
+    const currLocation = useLocation()
+
+    const isHomePage = currLocation.pathname === "/"
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [search, setSearch] = useState(searchParams.get("search") || "")
+
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            if(search){
+                setSearchParams({search})
+            }
+
+            else{
+                setSearchParams({})
+            }
+        }, 400)
+
+        return () => clearTimeout(timer)
+    }, [search])
   return (
     <div>Navbar</div>
   )
