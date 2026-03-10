5704cb50d023b160040e19ae7271ee9e97ae5867
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-11 01:29:10 +0530

    created a protect route also

---
+++ frontend/src/components/protectedRoute.tsx
@@ -0,0 +1,25 @@
+import { useAppData } from "../context/AppContext";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+const ProtectedRoute=()=>{
+    const {isAuth,user, loading} = useAppData();
+
+    if(loading) return null;
+    const location = useLocation();
+
+    if(!isAuth){
+        return <Navigate to={"/login"} replace/>
+    }
+
+    if(user?.role === null && location.pathname !== "/select-role"){
+        return <Navigate to={"/select-role"} replace/>
+    }
+
+    if(!user?.role !== null && location.pathname === "/select-role"){
+        return <Navigate to={"/"} replace/>
+    }
+
+    return <Outlet />
+}   
+
+export default ProtectedRoute
