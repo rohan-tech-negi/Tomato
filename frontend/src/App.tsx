508c102b4598557084c54d1ba163d4c77bf12bc0
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-11 02:49:53 +0530

    and applied the navbar in the main component also

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,9 +8,11 @@ import {Toaster} from 'react-hot-toast'
 import ProtectedRoute from './components/protectedRoute'
 import PublicRoute from './components/publicRoute'
 import SelectRole from './pages/SelectRole'
+import Navbar from './components/navbar'
 const App = () => {
   return (
     <BrowserRouter>
+    <Navbar></Navbar>
       <Routes>
         <Route element={<PublicRoute></PublicRoute>}>
         <Route path="/login" element={<Login></Login>} />
