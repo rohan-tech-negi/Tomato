a4467d0c6b310582f517b648d6f10551f825cd79
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-13 00:43:55 +0530

    setup the folder is completed now working on different files

--- services/restaurant/src/index.ts
+++ services/restaurant/src/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import connectDB from "./config/db.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -6,4 +10,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();    
 });
