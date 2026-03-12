4a1628c4edbc8383292409ec8995b0f9d90d4434
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-12 14:39:06 +0530

    done with the account page logic now moving to the ui part

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
-
+import { useAppData } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
 export const Account = () => {
+  const {user, setUser, setIsAuth} = useAppData()
+
+  const firstLetter = user?.name.charAt(0).toUpperCase()
+
+  const navigate = useNavigate()
+
+  const logoutHandler =()=>{
+    localStorage.setItem("token", "")
+    setUser(null)
+    setIsAuth(false)
+    navigate("/login")
+    toast.success("Logout successfully")
+    
+  }
   return (
     <div>Account</div>
   )
