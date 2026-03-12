c7619fc42e67ebf61869f06e8577d9bfd93b5fc1
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-13 00:37:04 +0530

    done with all the packages setup now working on the folder

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -47,25 +47,25 @@ export const AppProvider = ({children} : AppProviderProps)=>{
         setLoadingLocation(true)
         navigator.geolocation.getCurrentPosition(async (position)=>{
             const {latitude, longitude} = position.coords;
-            try {
+                try {
                 const res = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`)
-                const data = await res.json();
-                setLocation({
-                    latitude,
-                    longitude,
-                    formattedAddress: data.display_name || "current location"
-                });
+                    const data = await res.json();
+                    setLocation({
+                        latitude,
+                        longitude,
+                        formattedAddress: data.display_name || "current location"
+                    });
                 setCity(data.address.city || data.address.town || data.address.village || "Your location");
                 setLoadingLocation(false);  
-            } catch (error) {
-                setLocation({
-                    latitude,
-                    longitude,
-                    formattedAddress: "current location"
+                } catch (error) {
+                    setLocation({
+                        latitude,
+                        longitude,
+                        formattedAddress: "current location"
                 })
                 setCity("Failed to load")
                 
-            }
+                }
         })
     },[])
 
