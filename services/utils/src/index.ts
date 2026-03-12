83b43d4e9774ca929877e48a3e43474e9d595f0e
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-13 01:11:50 +0530

    done with all the setup of the utils folder

--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import connectDB from "./config/db.js";
+
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    connectDB();    
+       
 });
