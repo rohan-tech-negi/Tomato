2fa249cadbb6118b314468e3088d066b2213cc7f
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-12 14:34:03 +0530

    starting with todays work by working on the account page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,7 @@ const App = () => {
         <Route element={<ProtectedRoute></ProtectedRoute>}>
          <Route path="/" element={<Home />} />
          <Route path='/select-role' element={<SelectRole></SelectRole>}></Route>
+         
         </Route>
 
        
