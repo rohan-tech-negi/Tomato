d7f230d435471933342cdb3b43149845f9e3aec1
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-16 17:20:36 +0530

    working on a controller restaurant that check and monitor the restaurant working

--- services/restaurant/src/controller/restaurant.ts
+++ services/restaurant/src/controller/restaurant.ts
@@ -1,8 +1,31 @@
 import { AuthenticatedRequest } from "../middleware/isAuth.js";
 import TryCatch from "../middleware/trycatch.js";
+import Restaurant from "../models/Restaurant.js";
 
 export const addRestaurant = TryCatch(async(req:AuthenticatedRequest,res)=>{
     const user = req.user;
 
-    
+    if(!user){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    const existingRestaurant = await Restaurant.findOne({
+        ownerId: user._id,
+    })
+
+    if(existingRestaurant){
+        return res.status(400).json({
+            message: "You already have a restaurant",
+        })
+    }
+
+    const {name, description, latitude, longitude, formattedAddress, phone} = req.body;
+
+    if(!name || !latitude || !longitude){
+        return res.status(400).json({
+            message: "Please provide all the required fields",
+        })
+    }
 })
