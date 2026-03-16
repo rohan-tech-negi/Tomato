165d7ffa096784a61841ff7a02098fc9d34fde77
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-16 17:43:22 +0530

    working on the frontend with the restaurant page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,15 @@ import PublicRoute from './components/publicRoute'
 import SelectRole from './pages/SelectRole'
 import Navbar from './components/navbar'
 import { Account } from './pages/Account'
+import { useAppData } from './context/AppContext'
+import type Restaurant from './pages/Restaurant'
 const App = () => {
+
+  const {user} = useAppData()
+
+  if(user && user.role === "seller"){
+    return <Restaurant></Restaurant>
+  }
   return (
     <BrowserRouter>
     <Navbar></Navbar>
