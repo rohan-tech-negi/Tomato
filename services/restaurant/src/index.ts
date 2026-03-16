878894c037beb402d3788e5d99f41e650ae78225
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-16 17:40:19 +0530

    added the route in the index.ts main file

--- services/restaurant/src/index.ts
+++ services/restaurant/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import connectDB from "./config/db.js";
-
+import restaurantRoutes from "./routes/restaurant.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+
+
+app.use(express.json())
+
+app.use("/api/restaurant", restaurantRoutes)
 
 app.listen(PORT, () => {
     console.log(`Restaurent Server is running on port ${PORT}`);
