f90243887c65e6541c554638dc71efae0d7f1be1
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-01 00:47:46 +0530

    implemented logic inside the addm,enuitems

--- frontend/src/components/AddMenuItem.tsx
+++ frontend/src/components/AddMenuItem.tsx
@@ -1,6 +1,50 @@
+import { useState } from "react"
+import { restaurantService } from "../main";
+import toast from "react-hot-toast";
 
 
 const AddMenuItem = () => {
+    const[name, setName] = useState("");
+    const[description, setDescription] = useState("");
+    const[price, setPrice] = useState("")
+    const [image, setImage] = useState<File | null>(null);
+    const [loading, setLoading] = useState(false)
+
+    const resetForm = ()=>{
+        setName("")
+        setDescription("");
+        setPrice("")
+        setImage(null)
+    }
+
+    const handleSubmit = async()=>{
+        if(!name || !price || !image){
+            alert(("Name price and image is required"))
+            return ;
+        }
+
+        const formData = new FormData();
+
+        formData.append("name", name)
+        formData.append("description", description)
+        formData.append("price", price)
+        formData.append("file", image)
+
+        try {
+            setLoading(true)
+            await axios.post(`${restaurantService}/api/item/new`, formData, {
+                headers:{
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+            })
+            toast.success(("Item added succesfully"))
+            resetForm()
+            onItemAdded()
+        } catch (error) {
+            console.log(error)
+            toast.error("failed to add items")
+        }
+    }
   return (
     <div>
       
