064e3d0ce666e4d74d6b00d9bbd7b1f0ed742ce4
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-02 04:11:27 +0530

    things are working perfectly with the get menu items

--- frontend/src/components/MenuItems.tsx
+++ frontend/src/components/MenuItems.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { IMenuItems } from "../types"
 
 
@@ -8,9 +9,23 @@ interface MenuItemProps{
 }
 
 const MenuItems = ({items, onItemDeleted, isSeller}: MenuItemProps) => {
+  const[loadingItemId, setLoadingItemId] = useState<string | null>(null)
   return (
-    <div className="">
-      
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {
+          items.map((item)=>{
+            
+            const isLoading = loadingItemId === items._id;
+
+            return <div className={`relative flex gap-4 rounded-lg bg-white p-4 shadow-sm transition ${!item.isAvailable ? "opacity-50" : ""}`}>
+              <div className="relative shrink-0">
+                <img src={item.image} alt={item.name} className={`h-24 w-24 rounded-lg object-cover ${!item.isAvailable? "grayscale brightness-75" : ""}`}/>
+                
+              </div>
+
+            </div>
+          })
+        }
     </div>
   )
 }
