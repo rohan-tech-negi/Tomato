29e996ba35b2b6b431f9393fa4e303c33ff205a2
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-02 04:19:46 +0530

    make the ui display of the menu items

--- frontend/src/components/MenuItems.tsx
+++ frontend/src/components/MenuItems.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import type { IMenuItems } from "../types"
+import { BsEye } from "react-icons/bs";
+import { FiEyeOff } from "react-icons/fi";
+import { BiTrash } from "react-icons/bi";
 
 
 interface MenuItemProps{
@@ -20,7 +23,40 @@ const MenuItems = ({items, onItemDeleted, isSeller}: MenuItemProps) => {
             return <div className={`relative flex gap-4 rounded-lg bg-white p-4 shadow-sm transition ${!item.isAvailable ? "opacity-50" : ""}`}>
               <div className="relative shrink-0">
                 <img src={item.image} alt={item.name} className={`h-24 w-24 rounded-lg object-cover ${!item.isAvailable? "grayscale brightness-75" : ""}`}/>
-                
+                {
+                  !item.isAvailable && (<span className="absolute inset-0 flex items-center justify-center rounded bg-black/60 text-xs font-semibold text-white">
+                      Not Available
+                  </span>
+          )}
+              </div>
+
+              <div className="flex flex-1 flex-col justify-between ">
+                <div >
+                  <h3 className="font-semibold">{item.name}</h3>
+                  {
+                    item.description && (
+                      <p className="text-sm text-gray-500 line-clamp-2">{item.description}</p>
+                    )
+                  }
+                </div>
+
+                <div className="flex items-center justify-between">
+                    <p className="font-medium text-lg">
+                      ${item.price}
+                    </p>
+                    {isSeller && (
+                      <div>
+                        <button className="flex gap-4 rounded-md bg-red-500 px-3 py-1 text-white hover:bg-red-600" onClick={()=>{}} >
+                        {item.isAvailable ? <BsEye size={18}></BsEye> : <FiEyeOff size={18}></FiEyeOff>}
+                      </button>
+
+                      <button onClick={(()=>{})} className="rounded-lg p-2 text-red-500 hover: bg-red-50 ">
+                        <BiTrash size={18}></BiTrash>
+                      </button>
+                      </div>
+                      
+                    )}
+                </div>
               </div>
 
             </div>
