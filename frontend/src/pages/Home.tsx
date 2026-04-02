90ab2207b1b12255ead5450b855dbfc477d5ea16
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-03 00:29:05 +0530

    working on the home route to render al the restaurants form the customer

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+// import React from 'react'
+
+import { useSearchParams } from "react-router-dom"
+import { useAppData } from "../context/AppContext"
+import { useState } from "react"
+import { IRestaurant } from "../types"
 
 const Home = () => {
+  const {location} = useAppData()
+
+  const{searchParams} = useSearchParams()
+
+  const search = searchParams.get("search") || ""
+
+  const[restaurants, setRestaurants] = useState<IRestaurant[]>([])
+
+  const[loading, setLoading] = useState(true);
+
+
+  
   return (
     <div>Home</div>
   )
