5c34bfee06818a7c18255221bd9dc5a856c016a3
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-03 00:25:37 +0530

    and alig all the controller inside the routes

--- services/restaurant/src/routes/restaurant.ts
+++ services/restaurant/src/routes/restaurant.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAuth, isSeller } from '../middleware/isAuth.js';
-import { addRestaurant, fetchMyRestaurant, updateRestaurant, updateStatusRestaurant } from '../controller/restaurant.js';
+import { addRestaurant, fetchMyRestaurant, fetchSingleRestaurant, getNearbyRestaurant, updateRestaurant, updateStatusRestaurant } from '../controller/restaurant.js';
 import upload from '../middleware/multer.js';
 
 const router = express.Router()
@@ -9,4 +9,6 @@ router.post("/new", isAuth, isSeller, upload , addRestaurant)
 router.get("/my", isAuth, isSeller, fetchMyRestaurant)
 router.put("/status", isAuth, isSeller, updateStatusRestaurant)
 router.put("/edit", isAuth, isSeller, updateRestaurant)
+router.get("/all", isAuth, getNearbyRestaurant)
+router.get("/:id", isAuth, fetchSingleRestaurant)
 export default router;
