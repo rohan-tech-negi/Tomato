7e19c6666e31bfa78c662dcc300e4dba42c3c076
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-03 00:43:54 +0530

    applied the formula to find the longitude and the latitude

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -17,8 +17,16 @@ const Home = () => {
 
   const[loading, setLoading] = useState(true);
 
-  const getDistanceKm = (lat1:number, lon1: number)
+  const getDistanceKm = (lat1:number, lon1: number, lat2: number, lon2: number): number=>{
 
+    const R = 6371;
+    const dLat = (lat2 - lat1) * Math.PI / 180;
+    const dLon = (lon2 - lon1) * Math.PI / 180;
+
+    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+}
   return (
     <div>Home</div>
   )
