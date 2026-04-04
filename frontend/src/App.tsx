d112689ed277c5b56803b8d6785cedcb78eeb152
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-05 02:23:58 +0530

    working on the new daya with making the restuarnt page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import Navbar from './components/navbar'
 import { Account } from './pages/Account'
 import { useAppData } from './context/AppContext'
 import  Restaurant from './pages/Restaurant'
+import RestaurantPage from './pages/RestaurantPage'
 const App = () => {
 
   const {user} = useAppData()
@@ -34,6 +35,7 @@ const App = () => {
 
         <Route element={<ProtectedRoute></ProtectedRoute>}>
          <Route path="/" element={<Home />} />
+         <Route path='/restaurant/:id' element={<RestaurantPage></RestaurantPage>}></Route>
          <Route path='/select-role' element={<SelectRole></SelectRole>}></Route>
          <Route path='/account' element={<Account></Account>}></Route>
         </Route>
