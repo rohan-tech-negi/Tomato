96b8bc1d894e4c056605e862284a05b7d99999aa
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-05 02:30:39 +0530

    as working on the logic of the restaurantpage

--- frontend/src/pages/RestaurantPage.tsx
+++ frontend/src/pages/RestaurantPage.tsx
@@ -1,6 +1,54 @@
 // import React from 'react'
 
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { IMenuItems, IRestaurant } from "../types"
+import axios from "axios"
+import { restaurantService } from "../main"
+
 const RestaurantPage = () => {
+  const{id} = useParams()
+
+  const[restaurant, setRestaurant] = useState<IRestaurant | null>(null)
+  const[MenuItems, setMenuItems] = useState<IMenuItems[]>([])
+  const[loading, setLoading] = useState(true)
+
+  const fetchRestaurant = async()=>{
+    try {
+      const {data} = await axios.get(`${restaurantService}/api/restaurants/${id}`, {
+        headers:{
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      setRestaurant(data || null)
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  const fetchMenuItems = async()=>{
+      try {
+        const {data} = await axios.get(`${restaurantService}/api/item/all/${id}`, 
+          {
+            headers:{
+              Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+          }
+        )
+        setMenuItems(data)
+      } catch (error) {
+        console.log(error)
+      }
+   }
+
+   useEffect(()=>{
+    if(id){
+      fetchRestaurant()
+      fetchMenuItems()
+    }
+   })
   return (
     <div>RestaurantPage</div>
   )
