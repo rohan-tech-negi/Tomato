996d245779aabf55f61d9f1e2863e10c726f1079
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-07 12:55:41 +0530

    as used that controller in the routes

--- services/restaurant/src/routes/cart.ts
+++ services/restaurant/src/routes/cart.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { isAuth } from "../middleware/isAuth.js";
-import { addCart } from "../controller/cart.js";
+import { addCart, fetchMyCart } from "../controller/cart.js";
 
 const router = express.Router()
 
 router.post("/add", isAuth, addCart)
+router.get("/all", isAuth, fetchMyCart)
 
 export default router;
