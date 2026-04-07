81c0434598348f17decafe9a26ad4025fe0a96dc
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-07 12:35:21 +0530

    making a seperate contrroller for the addcart feature for the add menu items

--- services/restaurant/src/controller/cart.ts
+++ services/restaurant/src/controller/cart.ts
@@ -1,1 +1,14 @@
-export const 
+import { AuthenticatedRequest } from "../middleware/isAuth.js";
+import TryCatch from "../middleware/trycatch.js";
+
+export const addCart = TryCatch(async(req:AuthenticatedRequest, res)=>{
+    if(!req.user){
+        return res.status(401).json({
+            message:"Please login"
+        })
+    }
+
+    const userId = req.user._id;
+
+    const {restaurantId, itemId} = req.body
+})
