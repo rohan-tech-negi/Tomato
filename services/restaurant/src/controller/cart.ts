a02bb7b9d300b3d8dcbfa635c708309811ee885d
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-07 12:48:11 +0530

    adding a new controlller named fetchMyCart for fetching the cart from the db

--- services/restaurant/src/controller/cart.ts
+++ services/restaurant/src/controller/cart.ts
@@ -49,3 +49,15 @@ export const addCart = TryCatch(async(req:AuthenticatedRequest, res)=>{
 
     })
 })
+
+
+
+export const fetchMyCart = TryCatch(async(req: AuthenticatedRequest, res)=>{
+     if(!req.user){
+        return res.status(401).json({
+            message:"Please login"
+        })
+    }
+
+    const userId = req.user._id;
+})
