1ba489ced5e8ecbc9d5b6003f15c1b42d2ad5497
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-07 12:44:00 +0530

    done with all the operations of adding the item in the cart

--- services/restaurant/src/controller/cart.ts
+++ services/restaurant/src/controller/cart.ts
@@ -1,5 +1,7 @@
+import mongoose from "mongoose";
 import { AuthenticatedRequest } from "../middleware/isAuth.js";
 import TryCatch from "../middleware/trycatch.js";
+import Cart from "../models/Cart.js";
 
 export const addCart = TryCatch(async(req:AuthenticatedRequest, res)=>{
     if(!req.user){
@@ -11,4 +13,39 @@ export const addCart = TryCatch(async(req:AuthenticatedRequest, res)=>{
     const userId = req.user._id;
 
     const {restaurantId, itemId} = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(restaurantId) || !mongoose.Types.ObjectId.isValid(itemId)){
+        return res.status(400).json({
+            message: "Invalid restaurant and item id"
+        })
+    }
+
+    const cartFromDifferentRestaurant = await Cart.findOne({
+        userId,
+        restaurantId: {$ne: restaurantId}
+    })
+
+    if(cartFromDifferentRestaurant){
+        return res.status(400).json({
+            message: "You can order from only one restaurant at a time. Please clear your cart first to add items from this resturant"
+        })
+    }
+
+    const cartItem = await Cart.findOneAndUpdate(
+        {userId, restaurantId, itemId},
+        {
+            $inc: {quantity:1},
+            $setOnInsert:{userId, restaurantId, itemId}
+        },
+        {
+            upsert: true, 
+            new: true, 
+            setDefaultsOnInsert: true
+        }
+    )
+    return res.json({
+        message: "Item added to cart",
+        cart: cartItem
+
+    })
 })
