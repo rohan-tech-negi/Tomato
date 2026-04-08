dd3600db4a32a0e936b46146451ec4552415b568
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-08 14:06:56 +0530

    setup there route also

--- services/restaurant/src/routes/cart.ts
+++ services/restaurant/src/routes/cart.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { isAuth } from "../middleware/isAuth.js";
-import { addCart, fetchMyCart } from "../controller/cart.js";
+import { addCart, fetchMyCart, incrementCartItem , decrementCartItem, clearCart} from "../controller/cart.js";
 
 const router = express.Router()
 
 router.post("/add", isAuth, addCart)
 router.get("/all", isAuth, fetchMyCart)
+router.put("/inc", isAuth, incrementCartItem)
+router.put("/dec", isAuth, decrementCartItem)
+router.delete("/clear", isAuth, clearCart)
+
 
 export default router;
