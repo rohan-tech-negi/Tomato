12033d8730054f847940eceb41c930aec6739933
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-08 14:23:26 +0530

    working onn the cart page via applying the logic

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+// import React from 'react'
+
+import { useNavigate } from "react-router-dom"
+import { useAppData } from "../context/AppContext"
+import { useState } from "react"
 
 const Cart = () => {
+  const{cart, subtotal, Quantity, fetchCart} = useAppData()
+  const navigate = useNavigate()
+
+  const[loadingItemId, setLoadingItemId] = useState<string | null>(null)
+
+  const[clearingCart, setClearingCart] = useState<boolean>(false)
+
+  if(!cart || cart.length === 0){
+    return <div className="flex min-h-[60vh] item-center justify-center">
+        <p className="text-gray-500 text-lg">
+          Your cart is empty
+        </p>
+    </div>
+  }
+
+  const
   return (
     <div>Cart</div>
   )
