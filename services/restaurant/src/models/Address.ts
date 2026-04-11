d7157b59fa1c0283b0553865176d98af0ad97322
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-11 14:55:58 +0530

    done with the address schema

--- services/restaurant/src/models/Address.ts
+++ services/restaurant/src/models/Address.ts
@@ -34,19 +34,18 @@ const addressSchema = new Schema({
         type: {
             type: String,
             enum: ["Point"],
-            required: true
+            default: "Point"
         },
         coordinates:{
             type: [Number],
             required: true
         }
     },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    },
-    updatedAt:{
-        type: Date,
-        default: Date.now
-    }
+    
+}, {
+    timestamps: true
 })
+
+addressSchema.index({location: "2dsphere"})
+
+export default mongoose.model<IAddress>("Address", addressSchema)
