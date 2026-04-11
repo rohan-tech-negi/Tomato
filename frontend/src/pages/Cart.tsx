2e054d8a2700851031d5d060a6a5a0b1278159d6
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-11 14:05:04 +0530

    now working on the ui of the cartpage

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -79,8 +79,13 @@ const Cart = () => {
       setClearingCart(false)
     }
   }
+
+  const checkout = ()=>{
+    navigate("/checkout")
+  }
   return (
-    <div>Cart</div>
+    <div className="mx-auto max-w-5xl px-4 py-6 space-y-6">
+    </div>
   )
 }
 
