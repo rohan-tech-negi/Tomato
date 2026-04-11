73cb17213db44112739f2848dc24dd895655d3dc
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-11 14:53:01 +0530

    created the schema as well

--- services/restaurant/src/models/Address.ts
+++ services/restaurant/src/models/Address.ts
@@ -18,5 +18,35 @@ export interface IAddress extends Document {
 }
 
 const addressSchema = new Schema({
-    
+    userId:{
+        type: String,
+        required: true
+    },
+    mobile:{
+        type: Number,
+        required: true
+    },
+    formatterAddress:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: {
+            type: String,
+            enum: ["Point"],
+            required: true
+        },
+        coordinates:{
+            type: [Number],
+            required: true
+        }
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now
+    }
 })
