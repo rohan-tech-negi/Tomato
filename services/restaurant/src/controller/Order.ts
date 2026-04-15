3314b7d9a6965e475c53981ed91ec809833e2c25
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-15 14:06:25 +0530

    fetching all the items from the order models one by one

--- services/restaurant/src/controller/Order.ts
+++ services/restaurant/src/controller/Order.ts
@@ -1,3 +1,33 @@
+import { AuthenticatedRequest } from "../middleware/isAuth.js";
 import TryCatch from "../middleware/trycatch.js";
+import Address from "../models/Address.js";
 
-export const createOrder = TryCatch
+export const createOrder = TryCatch(async(req:AuthenticatedRequest, res)=>{
+    const user = req.user;
+
+    if(!user){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    const {paymentMethod, addressId} = req.body
+    if(!addressId){
+        return res.status(400).json({
+            message: "Address is required"
+        })
+    }
+
+    const address = await Address.findOne({
+        _id: addressId,
+        userId: user._id,
+    })
+
+    if(!address){
+        return res.status(404).json({
+            message: " Address not found"
+        })
+    }
+
+    const 
+})
