cda730775244b1a1c61750e3a70078e23f635e5a
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-15 13:53:44 +0530

    working on the orderschema

--- services/restaurant/src/models/Order.ts
+++ services/restaurant/src/models/Order.ts
@@ -46,3 +46,94 @@ export interface IOrder extends Document{
     
 }
 
+const OrderSchema = new Schema<IOrder>({
+    userId:{
+        type: String,
+        required: true,
+        index: true
+    },
+    restaurantId:{
+        type: String,
+        required: true,
+        index: true
+    },
+    restaurantName:{
+        type: String,
+        required: true
+    },
+    riderId:{
+        type: String,
+        default: null,
+        index: true
+    },
+    riderPhone:{
+        type: Number,
+        default: null
+    },
+    riderName:{
+        type: String,
+        default: null
+    },
+    distance:{
+        type: Number,
+        required: true
+    },
+    riderAmount:{
+        type: Number,
+        required: true
+    },
+    items:{
+        type: Array,
+        required: true
+    },
+    subtotal:{
+        type: Number,
+        required: true
+    },
+    deliveryFee:{
+        type: Number,
+        required: true
+    },
+    platformFee:{
+        type: Number,
+        required: true
+    },
+    totalAmount:{
+        type: Number,
+        required: true
+    },
+    addressId:{
+        type: String,
+        required: true
+    },
+    deliveryAddress:{
+        type: Object,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true
+    },
+    paymentMethod:{
+        type: String,
+        required: true
+    },
+    paymentStatus:{
+        type: String,
+        required: true
+    },
+    expiresAt:{
+        type: Date,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        required: true
+    },
+    updatedAt:{
+        type: Date,
+        required: true
+    }
+}, {timestamps: true})
+
+export default mongoose.model<IOrder>("Order", OrderSchema)
