94397c7a04bce8161e53f834b77e959422202027
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-15 13:47:27 +0530

    working on the IOrder interface

--- services/restaurant/src/models/Order.ts
+++ services/restaurant/src/models/Order.ts
@@ -1,1 +1,22 @@
-import mongoose from "mongoose";
+import mongoose, {Schema, Document} from "mongoose";
+
+
+export interface IOrder extends Document{
+    userId: string;
+    restaurantId: string;
+    restaurantName: string;
+    riderId? : string | null;
+    riderPhone: number | null;
+    riderName: string | null;
+
+    items:{
+        itemId: string;
+        name: string,
+        price: number;
+        quantity: number;
+    }[];
+
+    subtotal: number
+    deliveryFee: number;
+    platformFee
+}
