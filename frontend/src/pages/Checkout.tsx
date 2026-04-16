f368f0a43ef1b595f56b07465202b91371b41bf5
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-17 02:42:29 +0530

    working on setting some backend data fetching in the frontend via setting backend logic

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -1,3 +1,7 @@
+import axios from "axios"
+import { useAppData } from "../context/AppContext"
+import { useEffect, useState } from "react"
+import { restaurantService } from "../main"
 
 
 interface Address{
@@ -7,6 +11,43 @@ interface Address{
 }
 
 const Checkout = () => {
+  const{cart, subTotal, quantity} = useAppData()
+
+  const [addresses, setAddresses] = useState<Address[]>([])
+
+  const[selectedAddressId, setselectedAddressId] = useState<string| null>(null);
+
+  const [loadingAddress, setLoadingAddress] = useState(true)
+
+  const[ loadingRazorpay, setLoadingRazorpay] = useState(false)
+
+  const[ loadingStripe, setLoadingStripe] = useState(false)
+
+  const[ createOrder, setCreateOrder] = useState(false)
+
+  useEffect(()=>{
+    const fetchAddresses = async()=>{
+      if(!cart || cart.length === 0){
+        setLoadingAddress(false)
+        return;
+      }
+      try {
+        const {data} = await axios.get(`${restaurantService}/api/address/all`,{
+          headers:{
+             Authorization: `Bearer ${localStorage.getItem("token")}`
+          }
+        })
+        setAddresses(data || [])
+      } catch (error) {
+        console.log(error)
+      }finally{
+        setLoadingAddress(false)
+      }
+    }
+    fetchAddresses()
+  },[])
+
+  
   return (
     <div>Checkout</div>
   )
