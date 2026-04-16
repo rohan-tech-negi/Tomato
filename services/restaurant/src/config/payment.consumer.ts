d7a5c3241a343b932bbe12f085b9b2738d887daf
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-17 02:30:34 +0530

    as still be working on the consumer part of the payment with the starting of the payment of the consumer

--- services/restaurant/src/config/payment.consumer.ts
+++ services/restaurant/src/config/payment.consumer.ts
@@ -1,4 +1,31 @@
+import Order from "../models/Order.js";
 import {getChannel} from "./rabbitmq.js"
 
 
-export const 
+export const startPaymentConsumer = async()=>{
+    const channel = getChannel()
+
+    channel.consume(process.env.PAYMENT_QUEUE!, async(msg)=>{
+        if(!msg) return;
+
+        try {
+            const event  = JSON.parse(msg.content.toString())
+
+            if(event.type !== "PAYMENT_SUCCESS"){
+                channel.ack(msg)
+                return ;
+            }
+
+            const {orderId} = event.data;
+
+            const order = await Order.findOneAndUpdate(
+                {
+                    _id: orderId,
+                    paymentStatus: {$ne: "paid"}
+                }
+            )
+        } catch (error) {
+            
+        }
+    })
+}
