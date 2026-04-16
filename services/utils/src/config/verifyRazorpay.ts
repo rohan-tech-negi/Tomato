881247e6788fd1b130290e7bd3f079d953698178
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-16 14:35:18 +0530

    working on a payment procedures

--- services/utils/src/config/verifyRazorpay.ts
+++ services/utils/src/config/verifyRazorpay.ts
@@ -2,5 +2,14 @@ import crypto from "crypto"
 
 export const verifyRazorpaySignature = (
     orderId: string,
-    paymentId: string
-)
+    paymentId: string,
+    signature: string
+) => {
+    const body = `${orderId} | ${paymentId}`
+
+    const expectedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET!)
+    .update(body.toString())
+    .digest("hex")
+
+    return expectedSignature === signature
+}
