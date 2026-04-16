45be57d210ba384a55386d3a9a76b4d641578875
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-16 13:59:32 +0530

    worrking on setting up the payment gateway as razorpay

--- services/restaurant/src/routes/Order.ts
+++ services/restaurant/src/routes/Order.ts
@@ -1,5 +1,11 @@
 import express from "express"
+import { isAuth } from "../middleware/isAuth.js";
+import { createOrder, fetchOrderForPayment } from "../controller/Order.js";
 
 const router = express.Router()
+
+
+router.post("/new", isAuth, createOrder)
+router.post("/payment", fetchOrderForPayment)
 
 export default router;
