e617ea0625adfc83630bdb41ee7d1459fe73444f
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-16 13:45:47 +0530

    setup its routes for the order.ts

--- services/restaurant/src/index.ts
+++ services/restaurant/src/index.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import itemRoutes from "./routes/menuItem.js"
 import cartRoutes from "./routes/cart.js"
 import addressRoutes from "./routes/Address.js"
+import OrderRoutes from "./routes/Order.js"
 dotenv.config();
 
 const app = express();
@@ -20,6 +21,7 @@ app.use("/api/restaurant", restaurantRoutes)
 app.use("/api/item", itemRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/address", addressRoutes)
+app.use("/api/order", OrderRoutes)
 
 app.listen(PORT, () => {
     console.log(`Restaurent Server is running on port ${PORT}`);
